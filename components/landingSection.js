e02fd2787807d033b5983198ce39d40f773b81f5
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-11-21 23:54:41 +0100

    created landing section

---
+++ components/landingSection.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function landingSection() {
+    return (
+        <div>
+            <h>landing section</h>
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
-
+import LandingSection from "../components/landingSection"
 export default function Home() {
   return (
     <div>
       <h1>Portfolio</h1>
+      <LandingSection></LandingSection>
     </div>
   )
 }
