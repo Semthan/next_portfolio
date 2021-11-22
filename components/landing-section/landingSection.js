33792b8f03e065812bbdd9eaa2dacccce59bfacd
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-11-22 21:27:41 +0100

    added modal

--- components/landing-section/landingSection.js
+++ components/landing-section/landingSection.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react';
+import classes from './landingSection.module.css'
+import { Button, Modal } from 'react-bootstrap';
 
-export default function landingSection() {
+export default function LandingSection() {
+
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
         <div>
-            <h>landing section</h>
+            <Button variant="primary" onClick={handleShow}>
+                Launch demo modal
+            </Button>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Modal heading</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Woohoo, youre reading this text in a modal!</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Save Changes
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </div>
     )
 }
