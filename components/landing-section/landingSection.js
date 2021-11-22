6657f50a1e389e035e7c0d11a0d9579df4480043
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-11-22 21:55:17 +0100

    cleaned up

--- components/landing-section/landingSection.js
+++ components/landing-section/landingSection.js
@@ -1,34 +1,10 @@
-import React, { useState } from 'react';
-import { Button, Modal } from 'react-bootstrap';
+import React from 'react';
 
 export default function LandingSection() {
 
-
-    const [show, setShow] = useState(false);
-
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-
     return (
         <div>
-            <Button variant="primary" onClick={handleShow}>
-                Launch demo modal
-            </Button>
 
-            <Modal show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Modal heading</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>Woohoo, youre reading this text in a modal!</Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Close
-                    </Button>
-                    <Button variant="primary" onClick={handleClose}>
-                        Save Changes
-                    </Button>
-                </Modal.Footer>
-            </Modal>
         </div>
     )
 }
