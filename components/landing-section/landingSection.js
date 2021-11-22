22a666230c6a597fb85c8a1158c22acb24b09da4
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-11-23 00:11:51 +0100

    main structure

--- components/landing-section/landingSection.js
+++ components/landing-section/landingSection.js
@@ -1,10 +1,70 @@
 import React from 'react';
+import classes from './landingSection.module.css'
+import { Col, Container, Row } from 'react-bootstrap';
 
 export default function LandingSection() {
 
     return (
-        <div>
+        <Container fluid className="bg-light border border-5">
+            <Row className="gap-5 border border-2">
+                <Col>g</Col>
+                <Col>g</Col>
+                <Col>g</Col>
+                <Col>g</Col>
+                <Col>g</Col>
+                <Col>g</Col>
+                <Col>g</Col>
+                <Col>g</Col>
+                <Col>g</Col>
+                <Col>g</Col>
+                <Col>g</Col>
+                <Col>g</Col>
+            </Row>
+            <Row className="gap-5 border border-2">
+                <Col>b</Col>
+                <Col>b</Col>
+                <Col>b</Col>
+                <Col>b</Col>
+                <Col>b</Col>
+                <Col>b</Col>
+                <Col>b</Col>
+                <Col>b</Col>
+                <Col>b</Col>
+                <Col>b</Col>
+                <Col>b</Col>
+                <Col>b</Col>
+            </Row>
+            <Row className="gap-5 border border-2">
+                <Col>c</Col>
+                <Col>c</Col>
+                <Col>c</Col>
+                <Col>c</Col>
+                <Col>c</Col>
+                <Col>c</Col>
+                <Col>c</Col>
+                <Col>c</Col>
+                <Col>c</Col>
+                <Col>c</Col>
+                <Col>c</Col>
+                <Col>c</Col>
 
-        </div>
+            </Row >
+            <Row className="gap-5 border border-2">
+                <Col>c</Col>
+                <Col>c</Col>
+                <Col>c</Col>
+                <Col>c</Col>
+                <Col>c</Col>
+                <Col>c</Col>
+                <Col>c</Col>
+                <Col>c</Col>
+                <Col>c</Col>
+                <Col>c</Col>
+                <Col>c</Col>
+                <Col>c</Col>
+
+            </Row >
+
+        </Container>
     )
 }
