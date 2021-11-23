85b18df91fbfb59a0e58f425ea6cb103a3c4df80
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-11-23 13:56:42 +0100

    landing section styling

--- components/landing-section/landingSection.js
+++ components/landing-section/landingSection.js
@@ -6,64 +6,33 @@ import { Col, Container, Row } from 'react-bootstrap';
 export default function LandingSection() {
 
     return (
-        <Container fluid className="bg-light border border-5">
-            <Row className="gap-5 border border-2">
-                <Col xs={3}><Logo /></Col>
-                <Col>g</Col>
-                <Col>g</Col>
-                <Col>g</Col>
-                <Col>g</Col>
-                <Col>g</Col>
-                <Col>g</Col>
-                <Col>g</Col>
-                <Col>g</Col>
-                <Col>g</Col>
-            </Row>
-            <Row className="gap-5 border border-2">
-                <Col>b</Col>
-                <Col>b</Col>
-                <Col>b</Col>
-                <Col>b</Col>
-                <Col>b</Col>
-                <Col>b</Col>
-                <Col>b</Col>
-                <Col>b</Col>
-                <Col>b</Col>
-                <Col>b</Col>
-                <Col>b</Col>
-                <Col>b</Col>
-            </Row>
-            <Row className="gap-5 border border-2">
-                <Col>c</Col>
-                <Col>c</Col>
-                <Col>c</Col>
-                <Col>c</Col>
-                <Col>c</Col>
-                <Col>c</Col>
-                <Col>c</Col>
-                <Col>c</Col>
-                <Col>c</Col>
-                <Col>c</Col>
-                <Col>c</Col>
-                <Col>c</Col>
-
-            </Row >
-            <Row className="gap-5 border border-2">
-                <Col>c</Col>
-                <Col>c</Col>
-                <Col>c</Col>
-                <Col>c</Col>
-                <Col>c</Col>
-                <Col>c</Col>
-                <Col>c</Col>
-                <Col>c</Col>
-                <Col>c</Col>
-                <Col>c</Col>
-                <Col>c</Col>
-                <Col>c</Col>
-
-            </Row >
-
-        </Container>
+        <Container fluid className="border border-5">
+            <header className={classes.hero}>
+                <Row className="border border-2">
+                    <Col xs={3}>
+                        <Logo />
+                    </Col>
+                    <Col>g</Col>
+                </Row>
+                <Row className="align-items-end">
+                    <Col xs={1}>b</Col>
+                    <Col xs={5}>
+                        <h1 className={classes.h1}>HELLO! I'M SEMTHAN </h1>
+                        <h3 className={classes.h3}>I'M FULLSTACK DEVELOPER</h3>
+                        <p className={classes.p}>I am currently a student at Nackademin
+                            school. I am working towards degree in
+                            web developmnet
+                        </p>
+                    </Col>
+                    <Col xs={1}>b</Col>
+                    <Col xs={4}>
+                        <div className={classes.circle}></div>
+                    </Col>
+                </Row>
+                <Row className="align-items-end">
+                    <Col>b</Col>
+                </Row>
+            </header>
+        </Container >
     )
 }
