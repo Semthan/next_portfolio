f030c245e2013a3018691242604608a813e823ff
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-11-24 00:47:32 +0100

    created about section

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import LandingSection from "../components/landing-section/landingSection"
 export default function Home() {
   return (
     <div>
-      <h1>Portfolio</h1>
       <LandingSection></LandingSection>
     </div>
   )
