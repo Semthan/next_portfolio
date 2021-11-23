d8760acbc1c4fecbdbbcba4d30ae8425a3fac6c0
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-11-23 23:43:36 +0100

    navbar & landing section styling

--- components/landing-section/landingSection.js
+++ components/landing-section/landingSection.js
@@ -1,38 +1,34 @@
 import React from 'react';
 import classes from './landingSection.module.css'
-import Logo from './logo'
+import Nav from './navBar'
 import { Col, Container, Row } from 'react-bootstrap';
 
 export default function LandingSection() {
 
     return (
-        <Container fluid className="border border-5">
-            <header className={classes.hero}>
-                <Row className="border border-2">
-                    <Col xs={3}>
-                        <Logo />
-                    </Col>
-                    <Col>g</Col>
-                </Row>
-                <Row className="align-items-end">
-                    <Col xs={1}>b</Col>
-                    <Col xs={5}>
-                        <h1 className={classes.h1}>HELLO! I'M SEMTHAN </h1>
-                        <h3 className={classes.h3}>I'M FULLSTACK DEVELOPER</h3>
-                        <p className={classes.p}>I am currently a student at Nackademin
-                            school. I am working towards degree in
-                            web developmnet
-                        </p>
-                    </Col>
-                    <Col xs={1}>b</Col>
-                    <Col xs={4}>
-                        <div className={classes.circle}></div>
-                    </Col>
-                </Row>
-                <Row className="align-items-end">
-                    <Col>b</Col>
-                </Row>
+        <>
+            <Nav />
+            <header className={`d-flex ${classes.masthead}`}>
+                <Container className="px-4 px-lg-5">
+                    <Row className="align-items-center">
+                        <Col>
+                            <h1 className="mb-1">I'M SEMTHAN</h1>
+                            <h3 className="mb-3"><em>I'M FULLSTACK DEVELOPER</em></h3>
+                            <p>I am currently a student at Nackademin
+                                school. I am working towards degree in</p>
+                        </Col>
+                        <Col xs={3}></Col>
+                        <Col>
+                            <div className={classes.circle}></div>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <div>
+
+                        </div>
+                    </Row>
+                </Container>
             </header>
-        </Container >
+        </>
     )
 }
