8c7cea6e0aafec8939b953ea50b372e0b001b7d0
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-11-24 16:43:59 +0100

    removed modal and added project card

---
+++ components/projects-section/projectCard.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Image from 'next/image'
+import { Card, Button } from 'react-bootstrap'
+import pic from '../../public/images/cs.jpg'
+
+export default function ProjectCard() {
+    return (
+        <>
+            <Card>
+                <Image className="img-fluid rounded" src={pic} />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the bulk of the card's content
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+        </>
+    )
+}
