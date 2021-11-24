cd896bdbfb4f2457d71d2b69ba42302ca4442cc3
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-11-24 17:01:36 +0100

    blog section main structure

--- components/blog-Section/blogSection.js
+++ components/blog-Section/blogSection.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import classes from './blogSection.module.css'
+import { Container, Row, Col } from 'react-bootstrap'
 
 export default function BlogSection() {
     return (
-        <div>
+        <section className={`px-4 px-lg-5 ${classes.main}`}>
+            <Container>
+                <h2 className="text-uppercase text-white p-2 mb-5">blog</h2>
+                <Row className="justify-content-center">
+                    <Col xs={4} className="m-5">
 
-        </div>
+                    </Col>
+                    <Col xs={4} className="m-5">
+
+                    </Col>
+                    <Col xs={4} className="m-5">
+
+                    </Col>
+                </Row>
+                <p>More projects button..</p>
+            </Container>
+        </section >
     )
 }
