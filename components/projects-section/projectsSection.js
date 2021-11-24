5aa1fbf6da983b448e1c20c34cb04fd244d700cc
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-11-24 14:10:08 +0100

    added styling

--- components/projects-section/projectsSection.js
+++ components/projects-section/projectsSection.js
@@ -1,15 +1,16 @@
 import React from 'react'
+import classes from './ProjectsSection.module.css'
 import { Container, Row } from 'react-bootstrap'
 
 export default function ProjectsSection() {
     return (
-        <>
+        <section className={`px-4 px-lg-5 ${classes.main}`}>
             <Container className="px-4 px-lg-5">
-                <h2 className="text-center text-uppercase text-secondary mb-0">Projects</h2>
+                <h2 className="text-uppercase text-white p-2 mb-5">Projects</h2>
                 <Row className="">
 
                 </Row>
             </Container>
-        </>
+        </section >
     )
 }
