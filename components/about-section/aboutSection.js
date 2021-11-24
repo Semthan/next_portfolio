6934830e582a82eb9c12a26eb0adf253ab6b04bb
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-11-24 13:46:21 +0100

    few changes

--- components/about-section/aboutSection.js
+++ components/about-section/aboutSection.js
@@ -7,7 +7,7 @@ export default function AboutSection() {
         <section className={`px-4 px-lg-5 ${classes.main}`}>
             <Container>
                 <h2 className="text-uppercase text-white p-2 mb-5">About me</h2>
-                <Row>
+                <Row className="align-items-center">
                     <Col xs={1}></Col>
                     <Col>
                         <div className={classes.pic}></div>
@@ -18,10 +18,7 @@ export default function AboutSection() {
                             Sed ut convallis nunc. Aliquam porta laoreet lorem quis commodo. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec mattis leo ac vestibulum gravida. Morbi aliquet, est in interdum
                             congue, risus elit fermentum justo, eget dignissim magna ante non tellus. Quisque ut ornare est.
                         </p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras luctus odio id eleifend maximus. Donec tempor, mi quis vulputate fermentum, nunc sem lacinia nisi, vitae placerat diam nunc quis erat.
-                            Sed ut convallis nunc. Aliquam porta laoreet lorem quis commodo. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec mattis leo ac vestibulum gravida. Morbi aliquet, est in interdum
-                            congue, risus elit fermentum justo, eget dignissim magna ante non tellus. Quisque ut ornare est.
-                        </p>
+                        <p>Red more link!</p>
                     </Col>
                     <Col xs={1}></Col>
                 </Row>
