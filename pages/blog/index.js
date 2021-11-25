20c79a1c7dd2b2cf4fba3b388df8b74d08e97ede
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-11-25 19:16:19 +0100

    render all post

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,9 +1,56 @@
 import React from 'react'
+import styles from '../../styles/blog.module.css'
+import Link from 'next/link'
 
-export default function Blog() {
+
+import { getAllPostsForHome } from "../../lib/api"
+import { Nav } from 'react-bootstrap'
+
+export default function Blog({ allPosts }) {
+
+    console.log(allPosts)
     return (
-        <div>
-            <h1>Blog</h1>
+        <div className={styles.container}>
+
+            <main className={styles.main}>
+
+                <div className={styles.grid}>
+                    {
+                        allPosts.length > 0 ? (
+                            allPosts.map((post) => (
+                                <div className={styles.card} key={post.sys.id}>
+                                    <div className={styles.imageHolder}>
+                                        <img src={post.coverImage.url} alt={post.title} />
+                                    </div>
+                                    <div className={styles.details}>
+                                        <Link href={`posts/${post.sys.id}`} passHref>
+                                            <Nav.Link>{post.title} &rarr;</Nav.Link>
+                                        </Link>
+                                        <p>{post.excerpt}</p>
+                                    </div>
+                                </div>
+                            ))
+                        ) : (
+                            <div className={styles.card}>
+                                <p>No posts added!</p>
+                            </div>
+                        )
+                    }
+                </div>
+            </main>
+
+            <footer className={styles.footer}>
+                <p>Simple blog app</p>
+            </footer>
         </div>
     )
+}
+
+export async function getStaticProps({ preview = false }) {
+    let allPosts = (await getAllPostsForHome(preview)) ?? [];
+
+    return {
+
+        props: { preview, allPosts }
+    }
 }
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import AboutSection from "../components/about-section/aboutSection"
 import BlogSection from "../components/blog-Section/blogSection"
 import LandingSection from "../components/landing-section/landingSection"
 import ProjectsSection from "../components/projects-section/projectsSection"
+
 export default function Home() {
   return (
     <div>
