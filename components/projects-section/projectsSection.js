502abeae6971a8cd8041c68e5da0c6eab3a369b5
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-11-28 14:41:36 +0100

    fixed css import

--- components/projects-section/projectsSection.js
+++ components/projects-section/projectsSection.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import classes from './ProjectsSection.module.css'
+import classes from './projectsSection.module.css'
 import { Container, Row, Col } from 'react-bootstrap'
 import ProjectCard from './projectCard'
 
